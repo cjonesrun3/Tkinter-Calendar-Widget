58a0eb6aa3c8a63791220827f2d2ebdf14234bd6
cjonesrun3 <31106248+cjonesrun3@users.noreply.github.com>
2019-09-29 17:35:14 -0500

    Adding Tags to rows

--- TCalendar.py
+++ TCalendar.py
@@ -123,8 +123,10 @@ class TreeCalendar(ttk.Frame):
         self.year_frame = ttk.Frame(self)
         self.year_frame.configure(style='TFrame')
         self.year_label = ttk.Label(self.year_frame, textvariable=self.currently_displayed_year, style='TLabel')
-        self.year_forward_button = ttk.Button(self.year_frame, style='R.TButton')
-        self.year_back_button = ttk.Button(self.year_frame, style='L.TButton')
+        self.year_forward_button = ttk.Button(self.year_frame, style='R.TButton',
+                                              command=self._refresh_calendar_year_forward)
+        self.year_back_button = ttk.Button(self.year_frame, style='L.TButton',
+                                           command=self._refresh_calendar_year_back)
 
         self.year_back_button.grid(column=0, row=0)
         self.year_label.grid(column=1, row=0)
@@ -138,7 +140,8 @@ class TreeCalendar(ttk.Frame):
         self.month_label = ttk.Label(self.month_frame, textvariable=self.currently_displayed_month, style='TLabel')
         self.month_forward_button = ttk.Button(self.month_frame, style='R.TButton',
                                                command=self._refresh_calendar_month_forward)
-        self.month_back_button = ttk.Button(self.month_frame, style='L.TButton')
+        self.month_back_button = ttk.Button(self.month_frame, style='L.TButton',
+                                            command=self._refresh_calendar_month_back)
 
         self.month_back_button.grid(column=0, row=0)
         self.month_label.grid(column=1, row=0)
@@ -191,6 +194,10 @@ class TreeCalendar(ttk.Frame):
         self._initialize_calendar()
 
     def _initialize_calendar(self):
+        """
+        Initializes the calendar portion of the widget. Default is to set to current month and year
+
+        """
         current_year = self.date.today().year
         current_month = self.date.today().month  # Current numerical month
         current_written_month = self.date.today().strftime('%B')  # Current month written out
@@ -199,10 +206,10 @@ class TreeCalendar(ttk.Frame):
         self.currently_displayed_month.set(current_written_month)  # Sets label to current month
 
         # Below returns the first weekday of the month and number of days in the month
-        current_month = calendar.monthcalendar(current_year, current_month)
+        current_month_calendar = calendar.monthcalendar(current_year, current_month)
         """
-        e.g.
-        September 2019
+        e.g. of what current_month returns for September 2019:
+        
         [0, 0, 0, 0, 0, 0, 1]
         [2, 3, 4, 5, 6, 7, 8]
         [9, 10, 11, 12, 13, 14, 15]
@@ -210,11 +217,35 @@ class TreeCalendar(ttk.Frame):
         [23, 24, 25, 26, 27, 28, 29]
         [30, 0, 0, 0, 0, 0, 0]
         
+        Below begins the process of replacing 0's with previous and next months days
         """
+        previous_month_amount_of_days = 0  # Amount of days in previous month
+        if current_month - 1 == 0:  # Checks to ensure previous month isn't in previous year
+            previous_month_amount_of_days = calendar.monthrange(current_year - 1, 12)[1]
+            # Subtracts one from year, set month to December and returns only days
+        else:
+            previous_month_amount_of_days = calendar.monthrange(current_year, current_month - 1)[1]
+            # Returns days in previous month
 
-        for day in current_month:
-            print(day)
-            self.tree.insert('', 'end', values=(day))  # INSERTS DATA INTO TREEVIEW
+        number_of_zero_slots_for_beggining_of_month = current_month_calendar[0].count(0)  # See below
+        # Above counts 0's which allows us to know how many slots in the current month will need to be filled by
+        # Previous dates
+        for (i, item) in enumerate(current_month_calendar[0]):
+            if item == 0:
+                current_month_calendar[0][i] = (previous_month_amount_of_days -
+                                                number_of_zero_slots_for_beggining_of_month) + (i + 1)
+
+        beggining_of_next_month_days = 1  # Starts next month at one and adds 1 until 0's are gone
+        for (i, item) in enumerate(current_month_calendar[-1]):  # Use -1 to always get last list in current_month_cal.
+            if item == 0:
+                current_month_calendar[-1][i] = beggining_of_next_month_days
+                beggining_of_next_month_days += 1
+
+        for day in current_month_calendar:
+            if day == current_month_calendar[0]:
+                self.tree.insert('', 'end', values=(day), tag='FirstWeek')  # Inserts data into treeview
+            else:
+                self.tree.insert('', 'end', values=(day))  # Inserts data into treeview
 
 
     def _refresh_calendar_month_forward(self, event=None):
@@ -231,6 +262,28 @@ class TreeCalendar(ttk.Frame):
 
         month_forward_calendar = calendar.monthcalendar(year, month_forward_value)  # Builds calendar
 
+        previous_month_amount_of_days = 0  # Amount of days in previous month
+        if month_forward_value - 1 == 0:  # Checks to ensure previous month isn't in previous year
+            previous_month_amount_of_days = calendar.monthrange(year - 1, 12)[1]
+            # Subtracts one from year, set month to December and returns only days
+        else:
+            previous_month_amount_of_days = calendar.monthrange(year, month_forward_value - 1)[1]
+            # Returns days in previous month
+
+        number_of_zero_slots_for_beggining_of_month = month_forward_calendar[0].count(0)  # See below
+        # Above counts 0's which allows us to know how many slots in the current month will need to be filled by
+        # Previous dates
+        for (i, item) in enumerate(month_forward_calendar[0]):
+            if item == 0:
+                month_forward_calendar[0][i] = (previous_month_amount_of_days -
+                                                number_of_zero_slots_for_beggining_of_month) + (i + 1)
+
+        beggining_of_next_month_days = 1  # Starts next month at one and adds 1 until 0's are gone
+        for (i, item) in enumerate(month_forward_calendar[-1]):  # Use -1 to always get last list in current_month_cal.
+            if item == 0:
+                month_forward_calendar[-1][i] = beggining_of_next_month_days
+                beggining_of_next_month_days += 1
+
         for day in month_forward_calendar:
             self.tree.insert('', 'end', values=(day))  # Inserts data into treeview
 
@@ -239,32 +292,199 @@ class TreeCalendar(ttk.Frame):
         self.currently_displayed_year.set(year)  # Sets labels for users
 
     def _refresh_calendar_year_forward(self, event=None):
-        pass
+        self.tree.delete(*self.tree.get_children())
+        numerical_month = list(calendar.month_abbr).index(self.month_label.cget('text')[:3])  # Numerical value of month
+        year_forward_value = int(self.year_label.cget('text')) + 1  # Adds one to year
 
+        year_forward_calendar = calendar.monthcalendar(year_forward_value, numerical_month)  # Builds calendar
 
+        previous_month_amount_of_days = 0  # Amount of days in previous month
+        if numerical_month - 1 == 0:  # Checks to ensure previous month isn't in previous year
+            previous_month_amount_of_days = calendar.monthrange(year_forward_value - 1, 12)[1]
+            # Subtracts one from year, set month to December and returns only days
+        else:
+            previous_month_amount_of_days = calendar.monthrange(year_forward_value, numerical_month - 1)[1]
+            # Returns days in previous month
 
+        number_of_zero_slots_for_beggining_of_month = year_forward_calendar[0].count(0)  # See below
+        # Above counts 0's which allows us to know how many slots in the current month will need to be filled by
+        # Previous dates
+        for (i, item) in enumerate(year_forward_calendar[0]):
+            if item == 0:
+                year_forward_calendar[0][i] = (previous_month_amount_of_days -
+                                                number_of_zero_slots_for_beggining_of_month) + (i + 1)
 
+        beggining_of_next_month_days = 1  # Starts next month at one and adds 1 until 0's are gone
+        for (i, item) in enumerate(year_forward_calendar[-1]):  # Use -1 to always get last list in current_month_cal.
+            if item == 0:
+                year_forward_calendar[-1][i] = beggining_of_next_month_days
+                beggining_of_next_month_days += 1
 
+        for day in year_forward_calendar:
+            self.tree.insert('', 'end', values=(day))  # Inserts data into treeview
 
+        self.currently_displayed_year.set(year_forward_value)  # Sets label to year + 1
+
+    def _refresh_calendar_month_back(self, event=None):
+        self.tree.delete(*self.tree.get_children())
+        numerical_month = list(calendar.month_abbr).index(self.month_label.cget('text')[:3])  # Numerical value of month
+        year = int(self.year_label.cget('text'))  # Retrieves currently displayed year
+        month_back_value = int(numerical_month) - 1  # Subtracts one month to current month
+
+        if month_back_value == 0:  # Accounts for moving back year via months
+            month_back_value = 12  # Sets month to december
+            year = year - 1  # Sets year back one
+        else:
+            pass
+
+        month_back_calendar = calendar.monthcalendar(year, month_back_value)  # Builds calendar for month back
+
+        previous_month_amount_of_days = 0  # Amount of days in previous month
+        if numerical_month - 1 == 0:  # Checks to ensure previous month isn't in previous year
+            previous_month_amount_of_days = calendar.monthrange(year - 1, 12)[1]
+            # Subtracts one from year, set month to December and returns only days
+        else:
+            previous_month_amount_of_days = calendar.monthrange(year, month_back_value - 1)[1]
+            # Returns days in previous month
+
+        number_of_zero_slots_for_beggining_of_month = month_back_calendar[0].count(0)  # See below
+        # Above counts 0's which allows us to know how many slots in the current month will need to be filled by
+        # Previous dates
+        for (i, item) in enumerate(month_back_calendar[0]):
+            if item == 0:
+                month_back_calendar[0][i] = (previous_month_amount_of_days -
+                                               number_of_zero_slots_for_beggining_of_month) + (i + 1)
+
+        beggining_of_next_month_days = 1  # Starts next month at one and adds 1 until 0's are gone
+        for (i, item) in enumerate(month_back_calendar[-1]):  # Use -1 to always get last list in current_month_cal.
+            if item == 0:
+                month_back_calendar[-1][i] = beggining_of_next_month_days
+                beggining_of_next_month_days += 1
+
+        for day in month_back_calendar:
+            self.tree.insert('', 'end', values=(day))  # Inserts data into treeview
+
+        previous_written_month = calendar.month_name[month_back_value]  # Retrieves month name by value
+        self.currently_displayed_year.set(year)  # Sets label to year
+        self.currently_displayed_month.set(previous_written_month)  # Sets label to month
+
+    def _refresh_calendar_year_back(self, event=None):
+        self.tree.delete(*self.tree.get_children())  # Clears treeview
+        numerical_month = list(calendar.month_abbr).index(self.month_label.cget('text')[:3])  # Numerical value of month
+        year_back_value = int(self.year_label.cget('text')) - 1  # Subtracts year from current year
+
+        year_back_calendar = calendar.monthcalendar(year_back_value, numerical_month)  # Builds calendar
+
+        previous_month_amount_of_days = 0  # Amount of days in previous month
+        if numerical_month - 1 == 0:  # Checks to ensure previous month isn't in previous year
+            previous_month_amount_of_days = calendar.monthrange(year_back_value - 1, 12)[1]
+            # Subtracts one from year, set month to December and returns only days
+        else:
+            previous_month_amount_of_days = calendar.monthrange(year_back_value, numerical_month - 1)[1]
+            # Returns days in previous month
+
+        number_of_zero_slots_for_beggining_of_month = year_back_calendar[0].count(0)  # See below
+        # Above counts 0's which allows us to know how many slots in the current month will need to be filled by
+        # Previous dates
+        for (i, item) in enumerate(year_back_calendar[0]):
+            if item == 0:
+                year_back_calendar[0][i] = (previous_month_amount_of_days -
+                                               number_of_zero_slots_for_beggining_of_month) + (i + 1)
+
+        beggining_of_next_month_days = 1  # Starts next month at one and adds 1 until 0's are gone
+        for (i, item) in enumerate(year_back_calendar[-1]):  # Use -1 to always get last list in current_month_cal.
+            if item == 0:
+                year_back_calendar[-1][i] = beggining_of_next_month_days
+                beggining_of_next_month_days += 1
+
+        for day in year_back_calendar:
+            self.tree.insert('', 'end', values=(day))
+
+        self.currently_displayed_year.set(year_back_value)
 
 
     def _select_date(self, event):
-        selected_day = self.tree.item(self.tree.focus())
-        col = self.tree.identify_column(event.x)
+        selected_day = self.tree.item(self.tree.focus())  # Item clicked
+        col = self.tree.identify_column(event.x)  # North/South columns returns data from entire row
+        row = self.tree.identify_row(event.y)
+        print(selected_day.items())
+
+
+        # variable and changed based on selection. Otherwise, you can select a date from the previous month, but
+        # It will return a datetime with the month the user is currently on
+        numerical_month = list(calendar.month_abbr).index(self.month_label.cget('text')[:3])
+        numerical_year = int(self.year_label.cget('text'))
+
+        month_of_date_selected = 0  # Initializing as current displayed month
+        year_of_date_selected = 0  # Initializing as current displayed year
 
         if col == '#1':
+            print('column 1')
+            if selected_day['tags'][0] == 'FirstWeek':  # Checks first row for numbers from previous month
+                print('This is a first row variabl')
+                if selected_day['values'][0] > 7:
+                    month_of_date_selected = numerical_month - 1
+                    print('modifying')
+                    print(month_of_date_selected)
+            elif row == 'I006':  # Checks last row for number in next month
+                if selected_day['values'][0] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][0]
+
+
         elif col == '#2':
+            if row == 'I001':
+                if selected_day['values'][1] > 7:
+                    month_of_date_selected = numerical_month - 1
+            elif row == 'I006':
+                if selected_day['values'][1] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][1]
+
         elif col == '#3':
+            if row == 'I001':
+                if selected_day['values'][2] > 7:
+                    month_of_date_selected = numerical_month - 1
+            elif row == 'I006':
+                if selected_day['values'][2] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][2]
+
         elif col == '#4':
+            if row == 'I001':
+                if selected_day['values'][3] > 7:
+                    month_of_date_selected = numerical_month - 1
+            elif row == 'I006':
+                if selected_day['values'][3] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][3]
+
+
         elif col == '#5':
+            if row == 'I001':
+                if selected_day['values'][4] > 7:
+                    month_of_date_selected = numerical_month - 1
+            elif row == 'I006':
+                if selected_day['values'][4] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][4]
+
         elif col == '#6':
+            if row == 'I001':
+                if selected_day['values'][5] > 7:
+                    month_of_date_selected = numerical_month - 1
+            elif row == 'I006':
+                if selected_day['values'][5] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][5]
+
         elif col == '#7':
+            if row == 'I001':
+                if selected_day['values'][6] > 7:
+                    month_of_date_selected = numerical_month - 1
+            elif row == 'I006':
+                if selected_day['values'][6] < 7:
+                    month_of_date_selected = numerical_month + 1
             cell_value = selected_day['values'][6]
 
         print('cell_value = %s' % cell_value)
@@ -275,9 +495,16 @@ class TreeCalendar(ttk.Frame):
         by month abbreviation, so I splice the month label to the first three letters. The day is the selected_day
         that the user has clicked on. 
         """
-        numerical_month = list(calendar.month_abbr).index(self.month_label.cget('text')[:3])
-        output_date = self.date(int(self.year_label.cget('text')), int(numerical_month), cell_value)
+        print(month_of_date_selected)
+        print(year_of_date_selected)
+
+        if month_of_date_selected == 0:
+            month_of_date_selected = numerical_month
+        if year_of_date_selected == 0:
+            year_of_date_selected = numerical_year
+        output_date = self.date(year_of_date_selected, month_of_date_selected, cell_value)
         print(output_date)
+        # TODO Figure out return
 
 
 
